dbb3ef6da87e2b14921b2c8a7194f6162e4786b4
Vanessa McHale <tmchale@wisc.edu>
2017-02-18 13:25:57 -0600

    lib

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+//! Pacakge to create the `doggo` executable to compliment a woofer. 
+//!
+//! # Examples
+//!
+//! ```c
+//! $ doggo maxine
+//! maxine is a d*ng fine woofer.
+//! $ doggo jethro -m
+//! 11/10 excellent bork & tail wagging
+//! ```
+
+/// Given a doggo's name, return a `bool` asserting whether they are good
+pub fn is_good_dogg(name: String) -> bool {
+    return true;
+}
